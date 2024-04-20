e88692aa2f1a48adf2c1ec8301a6fe5c1a12961e
Dhaval Kumar <dhavalkumar741@gmail.com>
2024-04-21 01:45:58 +0530

    changes nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,6 @@ vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
 
-
 -- [[plugins: lazy.nvim]]
 local plugins = {
   { 
@@ -75,6 +74,20 @@ local plugins = {
   {
     'goolord/alpha-nvim',
   },
+  -- {
+  --   'nvim-lualine/lualine.nvim',
+  --   dependencies = { 'nvim-tree/nvim-web-devicons' }
+  -- },
+  -- {
+  --   'romgrk/barbar.nvim',
+  --   dependencies = {
+  --     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+  --     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  --   },
+  --   init = function() vim.g.barbar_auto_setup = false end,
+  --   opts = {},
+  --   version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  -- },
 }
 
 local opts = {}
@@ -169,6 +182,119 @@ require('gitsigns').setup()
 require('alpha').setup(require('alpha.themes.dashboard').config)
 
 
+-- [[cmp: completion]]
+-- ref: https://github.com/hrsh7th/nvim-cmp
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
+})
+
+
+-- [[lualine: statusline]]
+-- ref:
+-- require('lualine').setup {
+--   options = {
+--     icons_enabled = true,
+--     theme = 'auto',
+--     component_separators = { left = '', right = ''},
+--     section_separators = { left = '', right = ''},
+--     disabled_filetypes = {
+--       statusline = {},
+--       winbar = {},
+--     },
+--     ignore_focus = {},
+--     always_divide_middle = true,
+--     globalstatus = false,
+--     refresh = {
+--       statusline = 1000,
+--       tabline = 1000,
+--       winbar = 1000,
+--     }
+--   },
+--   sections = {
+--     lualine_a = {'mode'},
+--     lualine_b = {'branch', 'diff', 'diagnostics'},
+--     lualine_c = {'filename'},
+--     lualine_x = {'encoding', 'fileformat', 'filetype'},
+--     lualine_y = {'progress'},
+--     lualine_z = {'location'}
+--   },
+--   inactive_sections = {
+--     lualine_a = {},
+--     lualine_b = {},
+--     lualine_c = {'filename'},
+--     lualine_x = {'location'},
+--     lualine_y = {},
+--     lualine_z = {}
+--   },
+--   tabline = {},
+--   winbar = {},
+--   inactive_winbar = {},
+--   extensions = {}
+-- }
+
+-- [[neovim: reset cursor]]
+-- ref: https://github.com/alacritty/alacritty/issues/5450
+-- vim.api.nvim_create_autocmd("ExitPre", {
+--   group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+--   command = "set guicursor=a:hor20-blinkwait100-blinkoff100-blinkon100", -- a:hor20-Cursor", default = a:ver90
+--   desc = "Set cursor back to beam when leaving Neovim."
+-- }) 
+
+-- [[presistant save]]
+-- ref: https://toddknutson.bio/posts/how-to-enable-neovim-undo-backup-and-swap-files-when-switching-linux-groups/
+USER = os.getenv("USER")
+local curr_group = vim.fn.system("id -ng 2> /dev/null | tr -d '\n'")
+
+SWAPDIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim/swap//"
+BACKUPDIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim/backup//"
+UNDODIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim/undo//"
+
+if vim.fn.isdirectory(SWAPDIR) == 0 then
+	vim.fn.mkdir(SWAPDIR, "p", "0o700")
+end
+
+if vim.fn.isdirectory(BACKUPDIR) == 0 then
+	vim.fn.mkdir(BACKUPDIR, "p", "0o700")
+end
+
+if vim.fn.isdirectory(UNDODIR) == 0 then
+	vim.fn.mkdir(UNDODIR, "p", "0o700")
+end
+
+-- Enable swap, backup, and persistant undo
+vim.opt.directory = SWAPDIR
+vim.opt.backupdir = BACKUPDIR
+vim.opt.undodir = UNDODIR
+vim.opt.swapfile = true
+vim.opt.backup = true
+vim.opt.undofile = true
+
+-- Append backup files with timestamp
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local extension = "~" .. vim.fn.strftime("%Y-%m-%d-%H%M%S")
+		vim.o.backupext = extension
+	end,
+})
+
+
 -- [[sync clipboard: for wsl]]
 -- ref: https://www.reddit.com/r/neovim/comments/vxdjyb/neovim_in_wsl2_cant_copypaste_tofrom_system/
 vim.opt.clipboard = "unnamedplus"
@@ -187,3 +313,4 @@ if vim.fn.has("wsl") == 1 then
     cache_enabled = 0,
   }
 end
+
